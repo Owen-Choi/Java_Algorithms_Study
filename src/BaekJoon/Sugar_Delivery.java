b287d413acac2d991faedce97a05abb04da32cb1
Owen-Choi <oldstyle482@gmail.com>
2021-07-08 01:11:54 +0900

    백준 2839 : 설탕배달

---
+++ src/BaekJoon/Sugar_Delivery.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//2839번
+public class Sugar_Delivery {
+    static int input;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while(true){
+            if(input % 5 == 0){
+                System.out.println(input/5 + count);
+                break;
+            }
+            else if(input <= 0){
+                System.out.println(-1);
+                break;
+            }
+            input -= 3;
+            count++;
+        }
+    }
+
+
+}
