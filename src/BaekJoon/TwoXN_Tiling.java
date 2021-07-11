246797c555123b6c2cd785dda486abeb319c244a
Owen-Choi <oldstyle482@gmail.com>
2021-07-11 17:30:02 +0900

    백준 11726 : 2XN 타일링 (실패)

--- src/BaekJoon/TwoXN_Tiling.java
+++ src/BaekJoon/TwoXN_Tiling.java
@@ -7,30 +7,26 @@ import java.io.InputStreamReader;
 // 11726번
 public class TwoXN_Tiling {
     static int input;
-    static boolean flag[];
     static int TotalNum = 0;
+    static boolean[] flag;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         input = Integer.parseInt(br.readLine());
-        flag = new boolean[input * 2];
+        flag = new boolean[input];
         recur(0);
         System.out.println(TotalNum % 10007);
     }
+
     static void recur(int totalWidth){
+        int TempWidth = 0;
         if(totalWidth >= input) {
-            if(totalWidth == input)
+            if(totalWidth == input) {
                 TotalNum++;
+            }
             return;
         }
-        if(flag[totalWidth] == true){
-            totalWidth++;
-            recur(totalWidth);
-        }
-        else {
-            totalWidth += 2;
-            recur(totalWidth);
-            flag[totalWidth] = true;
-        }
-
+        recur(++totalWidth);
+        TempWidth = totalWidth += 2;
+        recur(TempWidth);
     }
 }
