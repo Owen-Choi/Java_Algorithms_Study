fe7effa69d41d0adbdeec88b73414373e7206668
Owen-Choi <oldstyle482@gmail.com>
2021-07-14 17:50:52 +0900

    백준 2579 : 계단 오르기 (시간초과)

--- src/BaekJoon/Climbing_Stairs.java
+++ src/BaekJoon/Climbing_Stairs.java
@@ -7,32 +7,34 @@ import java.io.InputStreamReader;
 // 2579번
 public class Climbing_Stairs {
     static int Input;
-    static int[] stairs;
-    static int TotalScore = 0;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Input = Integer.parseInt(br.readLine());
-        stairs = new int[Input+1];
+        arr = new int[Input + 3];
         for(int i=1; i<=Input; i++)
-            stairs[i] = Integer.parseInt(br.readLine());
-        int stepChecker = 0;
-        for(int i=0; i<Input;){
-            if(stepChecker < 2 && stairs[i+1] > stairs[i+2]){
-                TotalScore += stairs[++i];
-                stepChecker++;
-            }
-            else if(stepChecker < 2 && stairs[i+1] < stairs[i+2]){
-                i += 2;
-                TotalScore += stairs[i];
-                stepChecker = 0;
-            }
-            else if(stepChecker == 2){
-                i += 2;
-                TotalScore = stairs[i];
-                stepChecker = 0;
-            }
+            arr[i] = Integer.parseInt(br.readLine());
+
+        System.out.println(recur(0, 0, 0));
+    }
+
+    static int recur(int index, int totalCount, int stepCount){
+
+        if(index >= Input){
+            if(index == Input)
+                return totalCount;
+            else
+                return 0;
         }
-        TotalScore += stairs[Input];
-        System.out.println(TotalScore);
+
+        else if(stepCount == 2) {
+            int TemptotalCount = totalCount + arr[index + 2];
+            return recur(index + 2, TemptotalCount, 1);
+        }
+        else {
+            int Temp1 = totalCount + arr[index + 1];
+            int Temp2 = totalCount + arr[index + 2];
+            return Math.max(recur(index + 1, Temp1, ++stepCount), recur(index + 2, Temp2, 1));
+        }
     }
 }
