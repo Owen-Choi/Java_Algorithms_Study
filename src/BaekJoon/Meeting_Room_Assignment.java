bb5570539858a78d0b82998a0c83336d3165f0d6
Owen-Choi <oldstyle482@gmail.com>
2021-07-24 12:16:37 +0900

    백준 1931 : 회의실 배정(참고)

--- src/BaekJoon/Meeting_Room_Assignment.java
+++ src/BaekJoon/Meeting_Room_Assignment.java
@@ -3,39 +3,40 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 // 1931
 public class Meeting_Room_Assignment {
     static int RoomCount;
-    static Integer[] MR;
-    static int MeetingCount = 1;
+    static int[][] MR;
+    static int MeetingCount = 0;
     static int Min = Integer.MAX_VALUE;
     static int Max = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         RoomCount = Integer.parseInt(br.readLine());
-        MR = new Integer[RoomCount + 2];
-        int StartTime;
-        int EndTime;
+        MR = new int[RoomCount][2];
         for(int i=0; i<RoomCount; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            StartTime = Integer.parseInt(st.nextToken());
-            EndTime = Integer.parseInt(st.nextToken());
-            Min = Math.min(Min, EndTime);
-            Max = Math.max(Max, EndTime);
-            if(MR[StartTime] != null){
-                MR[StartTime] = Math.min(MR[StartTime], EndTime);
-            }
-            else
-                MR[StartTime] = EndTime;
+            MR[i][0] = Integer.parseInt(st.nextToken());
+            MR[i][1] = Integer.parseInt(st.nextToken());
         }
-        for(int i = Min; i<Max;){
-            if(MR[i] == null)
-                i++;
-            else {
+        Arrays.sort(MR, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                else
+                    return o1[1] - o2[1];
+            }
+        });
+        int EndTime = 0;
+        for(int i=0; i<RoomCount; i++){
+            if(EndTime <= MR[i][0]){
+                EndTime = MR[i][1];
                 MeetingCount++;
-                i = MR[i];
             }
         }
         System.out.println(MeetingCount);
