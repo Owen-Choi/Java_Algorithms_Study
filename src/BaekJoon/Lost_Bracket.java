7bef8387c8b65d2822d1d02b31793937cffb9a63
Owen-Choi <oldstyle482@gmail.com>
2021-07-25 18:25:25 +0900

    백준 1541 : 잃어버린 괄호(참고)

--- src/BaekJoon/Lost_Bracket.java
+++ src/BaekJoon/Lost_Bracket.java
@@ -6,48 +6,20 @@ import java.io.InputStreamReader;
 // 1541번
 public class Lost_Bracket {
     public static void main(String[] args) throws IOException {
+        int sum = Integer.MAX_VALUE;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String Input = br.readLine();
-        String newString = Input;
-        boolean isMinus = false;
-        for(int i=0; i<Input.length(); i++){
-            if(Input.charAt(i) == '-'){
-                isMinus = true;
-                continue;
+        String[] sub = br.readLine().split("-");
+        for(int i=0; i<sub.length; i++){
+            String[] mini = sub[i].split("\\+");
+            int temp = 0;
+            for(int k=0; k<mini.length; k++){
+                temp += Integer.parseInt(mini[k]);
             }
-            if(isMinus && Input.charAt(i) == '+'){
-                newString = newString.substring(0,i) + '-' + newString.substring(i+1);
-            }
-            if(isMinus && Input.charAt(i) == '-')
-                isMinus = false;
+            if(sum == Integer.MAX_VALUE)
+                sum = temp;
+            else
+                sum -= temp;
         }
-        System.out.println(newString);
-        int k=0;    int[] values = new int[newString.length()];
-        char[] arith = new char[newString.length()];
-        boolean SWM = false;
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<newString.length(); i++){
-            if(newString.charAt(i) != '+' && newString.charAt(i) != '-'){
-                sb.append(newString.charAt(i));
-            }
-            else{
-                if(i == 0){
-                    SWM = true;
-                    continue;
-                }
-
-                values[k] = Integer.parseInt(sb.toString());
-                sb.setLength(0);
-                arith[k] = newString.charAt(i);
-                k++;
-            }
-        }
-        values[k] = Integer.parseInt(sb.toString());
-            int result = values[0];
-            for (int i = 1; i < values.length; i++) {
-                result = arith[i - 1] == '+' ? result + values[i] : result - values[i];
-            }
-        result = SWM ? result - values[0] * 2 : result;
-        System.out.println(result);
+        System.out.println(sum);
     }
 }
