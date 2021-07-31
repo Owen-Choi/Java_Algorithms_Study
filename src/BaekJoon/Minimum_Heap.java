f45280a55c6c4735768ca018f36974ee50cc8abd
Owen-Choi <oldstyle482@gmail.com>
2021-07-31 19:03:33 +0900

    백준 1927 : 최소 힙(참고)

--- src/BaekJoon/Minimum_Heap.java
+++ src/BaekJoon/Minimum_Heap.java
@@ -1,86 +1,29 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.PriorityQueue;
 
 // 1927번
 public class Minimum_Heap {
     static int Input;
-    static int [] arr;
-    static int DataNum = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
         Input = Integer.parseInt(br.readLine());
-        arr = new int[Input + 1];
-        while(Input > 0){
-            Input--;
+        for(int i=0; i<Input; i++){
             int temp = Integer.parseInt(br.readLine());
             if(temp == 0){
-                if(isEmpty())
-                    System.out.println("0");
+                if(queue.isEmpty())
+                    bw.write("0\n");
                 else
-                    System.out.println(Delete());
+                    bw.write(queue.poll()+"\n");
             }
             else
-                Insert(temp);
+                queue.offer(temp);
         }
-    }
-    static boolean isEmpty(){
-        return DataNum == 0;
-    }
-    static int Delete() {
-        int tempNum;
-        tempNum = arr[1];
-        arr[1] = arr[DataNum--];
-        resize();
-        return tempNum;
-    }
-    static void Insert(int num) {
-        int tempIndex = ++DataNum;
-        if(tempIndex == 1) {
-            arr[1] = num;
-            return;
-        }
-        else
-            arr[tempIndex] = num;
-        while(tempIndex > 1){
-            if(arr[tempIndex] < arr[tempIndex/2]){
-                // swap
-                int temp = arr[tempIndex];
-                arr[tempIndex] = arr[tempIndex/2];
-                arr[tempIndex/2] = temp;
-                tempIndex /= 2;
-            }
-            else
-                break;
-        }
-    }
-    static void resize(){
-        if(DataNum == 0 || DataNum == 1)
-            return;
-        else{
-            int tempIndex = 1;
-            while(true){
-                int temp;
-                if(tempIndex * 2 > Input + 1)
-                    break;
-                if(arr[tempIndex] > arr[tempIndex * 2]){
-                    temp = arr[tempIndex];
-                    arr[tempIndex] = arr[tempIndex * 2];
-                    arr[tempIndex * 2] = temp;
-                    tempIndex *= 2;
-                    continue;
-                }
-                else if(arr[tempIndex] > arr[tempIndex * 2 + 1]){
-                    temp = arr[tempIndex];
-                    arr[tempIndex] = arr[tempIndex * 2 + 1];
-                    arr[tempIndex * 2 + 1] = temp;
-                    tempIndex = tempIndex * 2 + 1;
-                    continue;
-                }
-                break;
-            }
-        }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
