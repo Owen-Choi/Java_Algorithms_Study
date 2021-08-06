67dd20974c41bde97a523536e35cc4063ac3557a
Owen-Choi <oldstyle482@gmail.com>
2021-08-06 11:49:24 +0900

    백준 2178 : 미로 탐색(Comment 참고)

--- src/BaekJoon/Maze_Searching.java
+++ src/BaekJoon/Maze_Searching.java
@@ -9,54 +9,61 @@ public class Maze_Searching {
     static int N;
     static int M;
     static int [][] arr;
-    static boolean [][] check;
     static int Min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        check = new boolean[N][M];
+        arr = new int[N + 1][M + 1];
+        boolean[][] check = new boolean[N + 1][M + 1];
         for(int i = 0; i<N; i++){
             String line = br.readLine();
             for(int k = 0; k<M; k++){
                 arr[i][k] = Integer.parseInt(String.valueOf(line.charAt(k)));
             }
         }
-        recur(N - 1, M - 1, 0);
+        check[N-1][M-1] = true;
+        recur(N - 1, M - 1, 1, check);
         System.out.println(Min);
     }
-    static void recur(int raw, int col, int Count) {
+    static void recur(int raw, int col, int Count, boolean[][] check) {
         // 일단 범위에 속해야 확인을 함
-        if(raw + 1 <= N) {
-            if(arr[raw + 1][col] == 1 && !check[raw + 1][col]) {
+        if (raw + 1 <= N) {
+            if (arr[raw + 1][col] == 1 && !check[raw + 1][col]) {
                 check[raw + 1][col] = true;
-                Count++;
-                recur(raw + 1, col, Count);
+                System.out.println(raw + 1 + " " + col);
+                recur(raw + 1, col, ++Count, check);
+                check[raw + 1][col] = false;
             }
         }
-        if(col + 1 <= M) {
-            if(arr[raw][col + 1] == 1 && !check[raw][col + 1]) {
+        if (col + 1 <= M) {
+            if (arr[raw][col + 1] == 1 && !check[raw][col + 1]) {
                 check[raw][col + 1] = true;
-                Count++;
-                recur(raw, col + 1, Count);
+                System.out.println(raw + " " + (col + 1));
+                recur(raw, col + 1, ++Count, check);
+                check[raw][col + 1] = false;
             }
         }
-        if(raw - 1 >= 0) {
-            if(arr[raw - 1][col] == 1 && !check[raw - 1][col]) {
+        if (raw - 1 >= 0) {
+            if (arr[raw - 1][col] == 1 && !check[raw - 1][col]) {
                 check[raw - 1][col] = true;
-                Count++;
-                recur(raw - 1, col, Count);
+                System.out.println(raw - 1 + " " + col);
+                recur(raw - 1, col, ++Count, check);
+                check[raw - 1][col] = false;
             }
         }
-        if(col - 1 >= 0) {
-            if(arr[raw][col - 1] == 1 && !check[raw][col - 1]) {
+        if (col - 1 >= 0) {
+            if (arr[raw][col - 1] == 1 && !check[raw][col - 1]) {
                 check[raw][col - 1] = true;
-                Count++;
-                recur(raw, col - 1, Count);
+                System.out.println(raw + " " + (col - 1));
+                recur(raw, col - 1, ++Count, check);
+                check[raw][col - 1] = false;
             }
         }
+        else
+            return;
+
         if(col == 0 && raw == 0) {
             Min = Math.min(Min, Count);
         }
