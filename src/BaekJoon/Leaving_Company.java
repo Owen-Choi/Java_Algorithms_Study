99b19382c8639fd3945dc6bf693089ebcfc49a41
Owen-Choi <oldstyle482@gmail.com>
2021-08-10 14:18:12 +0900

    백준 14501 : 퇴사(참고)

--- src/BaekJoon/Leaving_Company.java
+++ src/BaekJoon/Leaving_Company.java
@@ -3,31 +3,33 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 // 14501번
 public class Leaving_Company {
-    static int Input;
-    static int[][] arr;
+    static int[] date;
+    static int[] value;
     static int[] dp;
-    static final int END_DATE = 0;
-    static final int VALUE = 1;
-    static int max = -1;
-    public static void main(String[] args) throws IOException {
+    static int Num;
+    static int Max = 0;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Input = Integer.parseInt(br.readLine());
-        arr = new int[Input + 1][2];
-        dp = new int[Input + 1];
-        dp[0] = 0;
-        for (int i = 1; i <= Input; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            arr[i][END_DATE] = Integer.parseInt(st.nextToken());
-            arr[i][VALUE] = Integer.parseInt(st.nextToken());
+        Num = Integer.parseInt(br.readLine());
+        date = new int[Num + 10];
+        value = new int[Num + 10];
+        dp = new int[Num + 10];
+        StringTokenizer st;
+        for(int i=0; i<Num; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            date[i] = Integer.parseInt(st.nextToken());
+            value[i] = Integer.parseInt(st.nextToken());
         }
-        for(int i=1; i<=Input; i++){
-            dp[i] = Math.max(dp[i], max);
-            dp[arr[i][END_DATE] + i]
+
+        for(int i=0; i<=Num; i++){
+            dp[i] = Math.max(dp[i], Max);
+            dp[i + date[i]] = Math.max(dp[i + date[i]], dp[i] + value[i]);
+            Max = Math.max(dp[i], Max);
         }
+        System.out.println(Max);
     }
 }
