329c1410c63e7a4549f8fd2cfcca0833916cfee6
Owen-Choi <oldstyle482@gmail.com>
2021-08-11 20:49:52 +0900

    백준 1018번 : 체스판 다시 칠하기(참고)

--- src/BaekJoon/RePaint_Chess_Plate.java
+++ src/BaekJoon/RePaint_Chess_Plate.java
@@ -1,64 +1,71 @@
 package BaekJoon;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-// 1018번
+import java.io.IOException;
+
 public class RePaint_Chess_Plate {
-    static int M;
-    static int N;
-    static int Min = Integer.MAX_VALUE;
-    static char[][] plate;
+
+    public static boolean[][] arr;
+    public static int min = 64;
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        plate = new char[M][N];
-        String temp;
-        for(int i = 0; i<M; i++){
-            temp = br.readLine();
-            for(int k=0; k<N; k++){
-                plate[i][k] = temp.charAt(k);
-            }
-        }
 
-        for(int i=0; i+7<M; i++){
-            for(int k=0; k+7<N; k++){
-                Check(i, k);
-            }
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        System.out.println(Min);
-    }
-    static void Check(int X_StartPos, int Y_StartPos){
-        int Change_Num = 0;
-        char[][] temp_plate = new char[plate.length][plate[0].length];
-        for(int i=0; i<plate.length; i++){
-            System.arraycopy(plate[i], 0, temp_plate[i], 0, plate[0].length);
-        }
-        for(int i=X_StartPos; i<X_StartPos + 8; i++){
-            for(int k=Y_StartPos; k<Y_StartPos + 7; k++){
-                if(temp_plate[i][k] == temp_plate[i][k+1]){
-                    Change_Num++;
-                    temp_plate[i][k+1] = temp_plate[i][k+1] == 'B' ? 'W' : 'B';
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new boolean[N][M];
+
+
+        // 배열 입력
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == 'W') {
+                    arr[i][j] = true;		// W일 때는 true
+                } else {
+                    arr[i][j] = false;		// B일 때는 false
                 }
+
             }
         }
 
-        for(int i=0; i<M; i++){
-            for(int k=0; k<N; k++){
-                System.out.print(temp_plate[i][k]);
+
+        int N_row = N - 7;
+        int M_col = M - 7;
+
+        for (int i = 0; i < N_row; i++) {
+            for (int j = 0; j < M_col; j++) {
+                find(i, j);
             }
-            System.out.println();
         }
+        System.out.println(min);
+    }
 
-        System.out.println();
-        System.out.println(Change_Num);
-        System.out.println("=========================================");
-        System.out.println();
 
-        Min = Math.min(Change_Num, Min);
+    public static void find(int x, int y) {
+        int end_x = x + 8;
+        int end_y = y + 8;
+        int count = 0;
+
+        boolean TF = arr[x][y];
+
+        for (int i = x; i < end_x; i++) {
+            for (int j = y; j < end_y; j++) {
+                if (arr[i][j] != TF) {
+                    count++;
+                }
+                TF = (!TF);
+            }
+            TF = !TF;
+        }
+        count = Math.min(count, 64 - count);
+        min = Math.min(min, count);
     }
 }
