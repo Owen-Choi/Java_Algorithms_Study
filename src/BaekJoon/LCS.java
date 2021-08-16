16ce908018ea3724f86ef4eab586627208f947c5
Owen-Choi <oldstyle482@gmail.com>
2021-08-16 14:16:26 +0900

    백준 9251 : LCS(참고)

--- src/BaekJoon/LCS.java
+++ src/BaekJoon/LCS.java
@@ -5,35 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class LCS {
-    static char[][] arr;
     static Integer[][] dp;
+    static char[] sen1;
+    static char[] sen2;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String temp = br.readLine();
-        arr = new char[temp.length()][2];
-        dp = new Integer[temp.length()][temp.length()];
-        for(int i=0; i<temp.length(); i++)
-            arr[i][0] = temp.charAt(i);
-        String temp1 = br.readLine();
-        for(int i=0; i<temp1.length(); i++)
-            arr[i][1] = temp1.charAt(i);
-        recur(temp.length() - 1, temp.length() - 1);
-        System.out.println(dp[temp.length() - 1][temp.length() - 1]);
+        sen1 = br.readLine().toCharArray();
+        sen2 = br.readLine().toCharArray();
+        dp = new Integer[sen1.length][sen2.length];
+        recur(sen1.length - 1, sen2.length - 1);
+        System.out.println(dp[sen1.length-1][sen2.length-1]);
     }
     static int recur(int raw, int col){
-        if(raw < 0 || col < 0){
+        if(raw < 0 || col < 0)
             return 0;
-        }
+
         if(dp[raw][col] == null){
             dp[raw][col] = 0;
-            for(int i=arr.length-1; i>=0; i--){
-                for(int k=arr.length-1; k>=0; k--){
-                    if(arr[i][0] == arr[k][1])
-                        dp[raw][col] = recur(raw - 1, col - 1) + 1;
-                    else
-                        dp[raw][col] = Math.max(recur(raw-1, col), recur(raw, col-1));
-                }
-            }
+            if(sen1[raw] == sen2[col])
+                dp[raw][col] = recur(raw-1, col - 1) + 1;
+            else
+                dp[raw][col] = Math.max(recur(raw-1, col), recur(raw, col-1));
         }
         return dp[raw][col];
     }
