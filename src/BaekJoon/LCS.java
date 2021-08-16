93a0f2f056013cc6c8003a0f4a5780112d5a9dc9
Owen-Choi <oldstyle482@gmail.com>
2021-08-16 13:59:14 +0900

    백준 9251 : LCS(코멘트 참고)

--- src/BaekJoon/LCS.java
+++ src/BaekJoon/LCS.java
@@ -6,35 +6,35 @@ import java.io.InputStreamReader;
 
 public class LCS {
     static char[][] arr;
-    static Integer[] dp;
+    static Integer[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String temp = br.readLine();
         arr = new char[temp.length()][2];
-        dp = new Integer[temp.length()];
+        dp = new Integer[temp.length()][temp.length()];
         for(int i=0; i<temp.length(); i++)
             arr[i][0] = temp.charAt(i);
         String temp1 = br.readLine();
         for(int i=0; i<temp1.length(); i++)
             arr[i][1] = temp1.charAt(i);
-
-        recur(arr.length-1, arr.length);
-        System.out.println(dp[arr.length-1]);
+        recur(temp.length() - 1, temp.length() - 1);
+        System.out.println(dp[temp.length() - 1][temp.length() - 1]);
     }
-    static int recur(int index, int index_Of_K){
-        if(index < 0)
+    static int recur(int raw, int col){
+        if(raw < 0 || col < 0){
             return 0;
-        if(dp[index] == null){
-            dp[index] = 0;   // 최솟값
-            for(int i = index; i>=0; i--){
-                for(int k=index_Of_K-1; k>=0; k--){
-                    if(arr[i][0] == arr[k][1]){
-                        System.out.println(arr[i][0]);
-                        dp[index] = Math.max(dp[index], recur(index-1, k) + 1);
-                    }
+        }
+        if(dp[raw][col] == null){
+            dp[raw][col] = 0;
+            for(int i=arr.length-1; i>=0; i--){
+                for(int k=arr.length-1; k>=0; k--){
+                    if(arr[i][0] == arr[k][1])
+                        dp[raw][col] = recur(raw - 1, col - 1) + 1;
+                    else
+                        dp[raw][col] = Math.max(recur(raw-1, col), recur(raw, col-1));
                 }
             }
         }
-        return dp[index];
+        return dp[raw][col];
     }
 }
