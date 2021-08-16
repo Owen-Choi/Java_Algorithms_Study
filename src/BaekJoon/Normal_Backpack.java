353b363a51b220875b8859a4083ea2a9175fa06f
Owen-Choi <oldstyle482@gmail.com>
2021-08-16 19:14:19 +0900

    백준 12865 : 평범한 배낭(구현 실패)

--- src/BaekJoon/Normal_Backpack.java
+++ src/BaekJoon/Normal_Backpack.java
@@ -8,40 +8,32 @@ import java.util.StringTokenizer;
 public class Normal_Backpack {
     static int N, K;
     static int[][] arr;
-    static Integer[] dp;
+    static Integer[][] dp;
     static final int W = 0;
     static final int V = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
-        arr = new int[N][2];
-        dp = new Integer[N];
-        dp[0] = 0;
         K = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < N; i++) {
+        arr = new int[N+1][K];
+        dp = new Integer[N+1][K];
+        for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             arr[i][W] = Integer.parseInt(st.nextToken());
             arr[i][V] = Integer.parseInt(st.nextToken());
         }
-        for(int i = N-1; i>=0; i--)
-            recur(i, 0);
-        int Max = -1;
-        for(int i=0; i<N; i++)
-            Max = Math.max(Max, dp[i]);
-        System.out.println(Max);
+        System.out.println(recur(N-1, K-1));
     }
-    static int recur(int index, int Weight){
-        if(index < 0)
+    static int recur(int index, int weight){
+        if(index < 0 || weight < 0)
             return 0;
-        int tempIndex = index-1;
-            dp[index] = 0;
-            if(arr[index][W] + Weight <= K){
-                dp[index] = Math.max(recur(tempIndex, (arr[index][W] + Weight)) + arr[index][V],
-                        recur(tempIndex, Weight));
-            }
+        if(dp[index][weight] == null){
+            if(arr[index][weight] > weight)
+                dp[index][weight] = recur(index-1, weight);
             else
-                dp[index] = Math.max(dp[index], recur(index-1, Weight));
-        return dp[index];
+                dp[index][weight] = Math.max(recur(index-1, weight), recur(index-1, weight - arr[index][W]) + arr[index][V]);
+        }
+        return dp[index][weight];
     }
 }
