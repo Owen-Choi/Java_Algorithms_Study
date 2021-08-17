0d980c501d46d994f64db96c1d9532dbcc3bca99
Owen-Choi <oldstyle482@gmail.com>
2021-08-17 16:09:36 +0900

    백준 12865 : 평범한 배낭(참고)

--- src/BaekJoon/Normal_Backpack.java
+++ src/BaekJoon/Normal_Backpack.java
@@ -7,32 +7,32 @@ import java.util.StringTokenizer;
 // 12865번
 public class Normal_Backpack {
     static int N, K;
-    static int[][] arr;
+    static int[] Weight;
+    static int[] Value;
     static Integer[][] dp;
-    static final int W = 0;
-    static final int V = 1;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][K];
-        dp = new Integer[N+1][K];
-        for (int i = 1; i <= N; i++) {
+        Weight = new int[N];
+        Value = new int[N];
+        dp = new Integer[N][K+1];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            arr[i][W] = Integer.parseInt(st.nextToken());
-            arr[i][V] = Integer.parseInt(st.nextToken());
+            Weight[i] = Integer.parseInt(st.nextToken());
+            Value[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(recur(N-1, K-1));
+        System.out.println(recur(N-1, K));
     }
     static int recur(int index, int weight){
-        if(index < 0 || weight < 0)
+        if(index < 0)
             return 0;
         if(dp[index][weight] == null){
-            if(arr[index][weight] > weight)
+            if(Weight[index] > weight)
                 dp[index][weight] = recur(index-1, weight);
             else
-                dp[index][weight] = Math.max(recur(index-1, weight), recur(index-1, weight - arr[index][W]) + arr[index][V]);
+                dp[index][weight] = Math.max(recur(index-1, weight), recur(index-1, weight - Weight[index]) + Value[index]);
         }
         return dp[index][weight];
     }
