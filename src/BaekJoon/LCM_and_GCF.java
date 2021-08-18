6226836e52f4755619dc107008e7f077da193374
Owen-Choi <oldstyle482@gmail.com>
2021-08-18 16:52:37 +0900

    백준 2609 : 최대공약수와 최소공배수(참고)

--- src/BaekJoon/LCM_and_GCF.java
+++ src/BaekJoon/LCM_and_GCF.java
@@ -13,30 +13,13 @@ public class LCM_and_GCF {
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         Num1 = Integer.parseInt(st.nextToken());
         Num2 = Integer.parseInt(st.nextToken());
-        int k = 0;
-        int temp;
-        while(true) {
-            temp = CM(Num1, Num2);
-            if(temp != -1)
-                arr[k++] = temp;
-            else
-                break;
-            Num1 /= temp;
-            Num2 /= temp;
-        }
-        temp = 1;
-        for(int i=0; i<k; i++) {
-            temp *= arr[i];
-        }
+        int temp = recur(Num1, Num2);
         System.out.println(temp);
-        System.out.println(temp * Num1 * Num2);
+        System.out.println(Num1 * Num2 / temp);
     }
-    static int CM(int n1, int n2){
-        int temp = Math.min(n1, n2);
-        for(int i = 2; i<temp; i++) {
-            if(n1 % i == 0 && n2 % i == 0)
-                return i;
-        }
-        return -1;
+    static int recur(int N1, int N2) {
+        if(N2 == 0)
+            return N1;
+        return recur(N2, N1%N2);
     }
 }
