005a7c2bbd42f1b524ed5f17df360ead11fa5967
Owen-Choi <oldstyle482@gmail.com>
2021-08-23 18:20:51 +0900

    백준 1966 : 프린터 큐(이론 참고)

--- src/BaekJoon/Printer_Queue.java
+++ src/BaekJoon/Printer_Queue.java
@@ -3,48 +3,65 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
+
 // 1966번
 public class Printer_Queue {
-    static int testCase, N, targetIndex;
-    static int [] arr;
-    public static void main(String[] args) throws IOException {
+    static int TestCase, PrintNum, TargetIndex;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        testCase = Integer.parseInt(br.readLine());
+        TestCase = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        LinkedList<Node> Origin_queue;
         StringBuilder sb = new StringBuilder();
-        PriorityQueue<Integer> pq;
-        int temp, Target = 0;
-        while(testCase --> 0) {
+        int temp, Count;   boolean Check;
+        while(TestCase --> 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            PrintNum = Integer.parseInt(st.nextToken());
+            TargetIndex = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine()," ");
-            N = Integer.parseInt(st.nextToken());
-            arr = new int[N];
-            targetIndex = Integer.parseInt(st.nextToken());
-            pq = new PriorityQueue<>(Comparator.reverseOrder());
-            st = new StringTokenizer(br.readLine()," ");
-            for(int i=0; i<N; i++) {
+            Origin_queue = new LinkedList<>();
+            Node tempNode;
+            for (int i = 0; i < PrintNum; i++) {
                 temp = Integer.parseInt(st.nextToken());
-                pq.offer(temp);
-                if(i == targetIndex)
-                    Target = temp;
+                Origin_queue.offer(new Node(i, temp));
             }
-            int Count = 0, ForSame = 0;
-            while(!pq.isEmpty()) {
-                temp = pq.poll();
-                if(temp == Target && ForSame == 0) {
-                    ForSame = Count;
+            Count = 0;
+            while(!Origin_queue.isEmpty()) {
+                Check = true;
+                tempNode = Origin_queue.poll();
+                for(int i=0; i<Origin_queue.size(); i++) {
+                    if(tempNode.Priority < Origin_queue.get(i).Priority) {
+                        Origin_queue.offer(tempNode);
+                        for(int k=0; k<i; k++)
+                            Origin_queue.offer(Origin_queue.poll());
+                        Check = false;
+                        break;
+                    }
+                }
+                if(!Check)
                     continue;
+                else {
+                    Count++;
+                    if(tempNode.Index == TargetIndex) {
+                        sb.append(Count).append('\n');
+                        break;
+                    }
                 }
-                else if(temp == Target) {
-                    ForSame++;
-                }
-                Count++;
             }
-            sb.append(ForSame).append('\n');
         }
         System.out.println(sb);
         br.close();
+    }
+
+    static class Node{
+        int Index;
+        int Priority;
+
+        public Node(int Ind, int Prio) {
+            Index = Ind;
+            Priority = Prio;
+        }
     }
 }
