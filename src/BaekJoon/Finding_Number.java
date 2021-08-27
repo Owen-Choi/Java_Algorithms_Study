48a6b280d7c918db98d6b5c2e04243f85d31c082
Owen-Choi <oldstyle482@gmail.com>
2021-08-27 16:58:53 +0900

    백준 1920 : 수 찾기(참고)

--- src/BaekJoon/Finding_Number.java
+++ src/BaekJoon/Finding_Number.java
@@ -1,50 +1,53 @@
 package BaekJoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 // 1920번
 public class Finding_Number {
     static int N, M;
-    static int[] Target, Input;
+    static int[] Target;
     static boolean check;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         N = Integer.parseInt(br.readLine());
         Target = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         for(int i=0; i<N; i++)
             Target[i] = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(br.readLine());
-        Input = new int[M];
         st = new StringTokenizer(br.readLine()," ");
-        for(int i=0; i<M; i++)
-            Input[i] = Integer.parseInt(st.nextToken());
+
         // 입력부 끝
 
         Arrays.sort(Target);
-        for(int i=0; i<N; i++) {
+        for(int i=0; i<M; i++) {
             check = false;
-            FindNum(Input[i], Target.length-1, 0, Target.length/2);
-            if(check)
-                System.out.println(1);
+            if(FindNum(Integer.parseInt(st.nextToken())) >= 0)
+                sb.append(1).append('\n');
             else
-                System.out.println(0);
+                sb.append(0).append('\n');
         }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
-    static void FindNum(int targetNum, int right, int left, int mid) {
-        if(left == mid) {
-            if(Target[left] == targetNum) {
-                check = true;
+    static int FindNum(int TargetNum) {
+        int left = 0, right = Target.length - 1, mid;
+        while(left <= right) {
+            mid = (left+right)/2;
+            if(TargetNum > Target[mid]) {
+                 left = mid + 1;
             }
-            else if(Target[right] == targetNum) {
-                check = true;
+            else if(TargetNum < Target[mid]) {
+                right = mid - 1;
             }
-            return;
+            else
+                return mid;
         }
-        FindNum(targetNum, mid, left, (mid+left)/2);
-        FindNum(targetNum, right, mid+1, (right+(mid+1))/2);
+    return -100;
     }
 }
