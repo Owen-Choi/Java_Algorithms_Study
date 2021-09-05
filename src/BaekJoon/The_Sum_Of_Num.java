54e362ae313a828e5c4cddd3d8250573cd352b46
Owen-Choi <oldstyle482@gmail.com>
2021-09-05 23:26:12 +0900

    백준 여러 문제들

---
+++ src/BaekJoon/The_Sum_Of_Num.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class The_Sum_Of_Num {
+    static long Num;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Num = Long.parseLong(br.readLine());
+        System.out.println(FindNuM());
+    }
+    static int FindNuM() {
+        int i = 1;
+        while(Num >= i)
+            Num -= i++;
+        return i-1;
+    }
+}
--- src/BaekJoon/Tomato.java
+++ src/BaekJoon/Tomato.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 public class Tomato {
     static int row, col;
     static int[][] arr;
+    static boolean[][] flag;
     static int result = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +17,7 @@ public class Tomato {
         col = Integer.parseInt(st.nextToken());
         row = Integer.parseInt(st.nextToken());
         arr = new int[row][col];
+        flag = new boolean[row][col];
         for(int i=0; i<row; i++){
             st = new StringTokenizer(br.readLine()," ");
             for(int k=0; k<col; k++) {
@@ -24,12 +26,14 @@ public class Tomato {
         }
         // 입력부 끝
         // 재귀를 돌리고, 0이 하나라도 있다면 -1 출력
-        borne(row-1, col-1, 0);
+        //borne(row-1, col-1, 0);
         for(int i=0; i<row; i++) {
             for(int k=0; k<col; k++){
-                System.out.print(arr[i][k] + " ");
+                if(arr[i][k] == 0) {
+                    System.out.println(-1);
+                    System.exit(0);
                 }
-            System.out.println();
+                }
             }
         System.out.println(result);
     }
@@ -41,15 +45,25 @@ public class Tomato {
         }
         if(arr[Srow][Scol] == 0) {
             arr[Srow][Scol] = 1;
+            flag[Srow][Scol] = true;
             newDays = days + 1;
+            borne(Srow - 1, Scol, newDays);
+            borne(Srow + 1, Scol, newDays);
+            borne(Srow, Scol - 1, newDays);
+            borne(Srow, Scol + 1, newDays);
         }
-        if(arr[Srow][Scol] == -1)
-            return;
+        else {
+            if (arr[Srow][Scol] == -1)
+                return;
+
+            if (flag[Srow][Scol])
+                return;
 
             borne(Srow - 1, Scol, newDays);
             borne(Srow + 1, Scol, newDays);
             borne(Srow, Scol - 1, newDays);
             borne(Srow, Scol + 1, newDays);
+        }
         result = Math.max(result, newDays);
     }
 }
