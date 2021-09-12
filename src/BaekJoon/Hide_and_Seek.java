a375af910edff89f05334d845de0429d882f9da9
Owen-Choi <oldstyle482@gmail.com>
2021-09-12 20:46:07 +0900

    백준 1697 : 숨바꼭질(이론 참고, 코드 참고)

--- src/BaekJoon/Hide_and_Seek.java
+++ src/BaekJoon/Hide_and_Seek.java
@@ -3,36 +3,48 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 // 1697번
+// 이제 그냥 bfs 하면 일단 queue부터 떠올리고 보자.
 public class Hide_and_Seek {
-    static int Subin, brother, result = Integer.MAX_VALUE;
+    static int Subin, brother, result;
+    static int[] dp;
+    static final int MAX = 100001;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         Subin = Integer.parseInt(st.nextToken());
         brother = Integer.parseInt(st.nextToken());
-        Solve(Subin, 0);
-        System.out.println(result);
+        System.out.println(Solve());
     }
-    static void Solve(int value, int time) {
-        if(time > result)
-            return;
-
-        if(value == brother) {
-            result = Math.min(result, time);
-            return;
+    static int Solve() {
+        int tempPos;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(Subin);
+        dp = new int[MAX];
+        dp[Subin] = 1;
+        while(!queue.isEmpty()) {
+            tempPos = queue.poll();
+            if(tempPos == brother) {
+                return dp[tempPos] - 1;
+            }
+            //무조건 먼저 들어가있는 값이 작게 되는 시스템이기 때문에 Math.min 함수를 사용할 필요가 없다.
+            if(tempPos <= MAX - 2 && dp[tempPos + 1] == 0) {
+                dp[tempPos + 1] = dp[tempPos] + 1;
+                queue.add(tempPos + 1);
+            }
+            if(tempPos >= 1 && dp[tempPos - 1] == 0) {
+                dp[tempPos - 1] = dp[tempPos] + 1;
+                queue.add(tempPos - 1);
+            }
+            if(tempPos*2 <= MAX - 1 && dp[tempPos * 2] == 0) {
+                dp[tempPos*2] = dp[tempPos] + 1;
+                queue.add(tempPos * 2);
+            }
         }
-        if(value > brother) {
-            if(time >= result)
-                return;
-            Solve(value - 1, time+1);
-        }
-        if(value <= 0 )
-            return;
-
-        Solve(value+1, time+1);
-        Solve(value*2, time+1);
-        Solve(value-1, time+1);
+        return -1;
     }
+
 }
