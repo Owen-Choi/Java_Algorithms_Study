8eadfc6053711166ffaf3dca375b1a7a69972d0f
Owen-Choi <oldstyle482@gmail.com>
2021-09-16 01:49:07 +0900

    백준 1504 : 특정한 최단경로(오답)

--- src/BaekJoon/Specific_Shortest_Route.java
+++ src/BaekJoon/Specific_Shortest_Route.java
@@ -4,33 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+import java.lang.Comparable;
 
 public class Specific_Shortest_Route {
     static int Vertex, Edge, EsenVer1, EsenVer2;
     static ArrayList<Node>[] list;
-    static PriorityQueue<Node> queue;
-    static final int INF = Integer.MAX_VALUE;
+    static final int INF = 200000000;
     static int[] distance;
+    static PriorityQueue<Node> queue;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
         Vertex = Integer.parseInt(st.nextToken());
         Edge = Integer.parseInt(st.nextToken());
-        list = new ArrayList[Vertex];
-        for(int i=0; i<Vertex; i++)
+        distance = new int[Vertex + 1];
+        list = new ArrayList[Vertex + 1];
+        for(int i=0; i<=Vertex; i++)
             list[i] = new ArrayList<>();
+        int tempDest, tempDist, tempStart;
         for(int i=0; i<Edge; i++) {
             st = new StringTokenizer(br.readLine()," ");
-            list[i].add(new Node(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1));
+            tempStart = Integer.parseInt(st.nextToken());
+            tempDest = Integer.parseInt(st.nextToken());
+            tempDist = Integer.parseInt(st.nextToken());
+            list[tempStart].add(new Node(tempDest, tempDist));
+            list[tempDest].add(new Node(tempStart, tempDist));
         }
         st = new StringTokenizer(br.readLine()," ");
         EsenVer1 = Integer.parseInt(st.nextToken());
         EsenVer2 = Integer.parseInt(st.nextToken());
-        // Input ends
+        // Input end
         Solve();
-
     }
     // Priority queue requires Comparable interface "" not Comparator interface
     static class Node implements Comparable<Node>{
@@ -44,24 +51,38 @@ public class Specific_Shortest_Route {
             return this.Distance - o.Distance;
         }
     }
-
-    static void Solve() {
+    static int Dijkstra(int StartIndex, int FinishIndex) {
+        Arrays.fill(distance, INF);
         queue = new PriorityQueue<>();
-        queue.add(new Node(EsenVer1-1, 0));
+        queue.add(new Node(StartIndex, 0));
         Node tempNode;
-        int tempDest, tempDistance;
+        int tempDest, tempDist;
         while(!queue.isEmpty()) {
             tempNode = queue.poll();
-            if(distance[tempNode.Destination] < tempNode.Distance)  continue;
             for(int i=0; i<list[tempNode.Destination].size(); i++) {
                 tempDest = list[tempNode.Destination].get(i).Destination;
-                tempDistance = list[tempNode.Destination].get(i).Distance + tempNode.Distance;
-                if(distance[tempNode.Destination] > tempDistance) {
-                    distance[tempNode.Destination] = tempDistance;
-                    queue.offer(new Node(tempDest, tempDistance))
+                tempDist = list[tempNode.Destination].get(i).Distance + tempNode.Distance;
+                if(distance[tempDest] > tempDist) {
+                    distance[tempDest] = tempDist;
+                    queue.add(new Node(tempDest, tempDist));
                 }
             }
         }
+        return distance[FinishIndex];
+    }
+    static void Solve() {
+        int value1 = 0;
+        value1 += Dijkstra(1, EsenVer1);
+        value1 += Dijkstra(EsenVer1, EsenVer2);
+        value1 += Dijkstra(EsenVer2, Vertex);
 
+        int value2 = 0;
+        value2 += Dijkstra(1, EsenVer2);
+        value2 += Dijkstra(EsenVer2, EsenVer1);
+        value2 += Dijkstra(EsenVer1, Vertex);
+        int result;
+
+        result = (value1 >= INF && value2 >= INF) ? -1 : Math.min(value1, value2);
+        System.out.println(result);
     }
 }
