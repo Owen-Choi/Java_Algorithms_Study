f59cdb5337a8794afe308845ec5745aa6ed698e5
Owen-Choi <oldstyle482@gmail.com>
2021-09-26 22:00:43 +0900

    백준 9372 : 상근이의 여행(이딴 것도 문제라고...)

---
+++ src/BaekJoon/World_Travel.java
@@ -0,0 +1,33 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class World_Travel {
+    static int testCase, Vertex, Edge;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        testCase = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int From, To;
+        while(testCase --> 0) {
+            st = new StringTokenizer(br.readLine()," ");
+            Vertex = Integer.parseInt(st.nextToken());
+            Edge = Integer.parseInt(st.nextToken());
+            arr = new int[Vertex + 1];
+            for(int i=0; i<Edge; i++) {
+                st = new StringTokenizer(br.readLine()," ");
+                From = Integer.parseInt(st.nextToken());
+                To = Integer.parseInt(st.nextToken());
+                arr[From] = To;
+                arr[To] = From;
+            }
+            sb.append(Vertex - 1).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
