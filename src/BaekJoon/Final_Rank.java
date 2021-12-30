f543760fd6d3670fc115f1349022d7983dad730d
Owen-Choi <oldstyle482@gmail.com>
2021-12-30 20:34:58 +0900

    백준 3665 : 최종순위(오답)

--- src/BaekJoon/Final_Rank.java
+++ src/BaekJoon/Final_Rank.java
@@ -3,10 +3,8 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+
 // 위상정렬 활용
 public class Final_Rank {
     static int testCase, teamNum;
@@ -41,8 +39,8 @@ public class Final_Rank {
                 temp2 = Integer.parseInt(st.nextToken());
                 Ranking_Change(temp1, temp2);
             }
-            // 예시 1번에서 답이 5 3 2 4 1인 이유는 2 4, 3 4 가
-            // 차례로 있지만 2 3은 없기 떄문에 2는 여전히 3보다 뒤에 존재한다.
+            Topoloy();
+            System.out.println();
         }
     }
     // 이름 그대로 순위를 바꾸는 함수
@@ -63,6 +61,38 @@ public class Final_Rank {
     static void Topoloy() {
         // 위상 정렬에서 팀을 선택하는 기준은 작년의 순위로 한다.
         Queue<Integer> queue = new LinkedList<>();
-
+        int[] result = new int[teamNum + 1];
+        Arrays.fill(result, -333);
+        for(int i=1; i<=teamNum; i++) {
+            if(t[i] == 0)
+                queue.offer(i);
+        }
+        int temp, counter = 0;
+        for(int i=1; i<=teamNum; i++) {
+            if(queue.size() == 0) {
+                System.out.println("IMPOSSIBLE");
+                return;
+            }
+            if(queue.size() > 1) {
+                System.out.println("?");
+                return;
+            }
+            temp = queue.poll();
+            result[counter++] = temp;
+            for(int j=1; j<=teamNum; j++) {
+                if(changed[temp][j]) {
+                    changed[temp][j] = false;
+                    if(--t[j] == 0)
+                        queue.offer(j);
+                }
+            }
+        }
+        // 출력
+        for(int i=0; i<teamNum; i++) {
+            if(result[i] == -333)
+                continue;
+            else
+                System.out.print(result[i] + " ");
+        }
     }
 }
