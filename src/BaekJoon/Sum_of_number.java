c289025b450313885d5c0853764cc7f13f808870
Owen-Choi <oldstyle482@gmail.com>
2023-01-14 22:40:08 +0900

    [ADD] 오랜만에 알고리즘

---
+++ src/BaekJoon/Sum_of_number.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 11720번
+public class Sum_of_number {
+    static String inputStr;
+    static int size;
+    static char[] convertedArr;
+    static int result = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        size = Integer.parseInt(br.readLine());
+        inputStr = br.readLine();
+        convertedArr = inputStr.toCharArray();
+        for (Character each : convertedArr) {
+            result += each - '0';
+        }
+        System.out.println(result);
+    }
+}
