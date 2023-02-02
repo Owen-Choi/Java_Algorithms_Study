6ed00ab0a830132ee3452b2ffe283b3bde1bf1a5
Owen-Choi <oldstyle482@gmail.com>
2023-02-02 22:16:23 +0900

    [ADD] 10819 차이를 최대로

---
+++ src/baekJoon/Cheese.java
@@ -0,0 +1,21 @@
+package baekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 2638번
+public class Cheese {
+    static int N;
+    static int M;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++) {
+
+        }
+    }
+}
