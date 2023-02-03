532b1e1eb5d263c54761edded7774121c1272dc6
Owen-Choi <oldstyle482@gmail.com>
2023-02-04 01:15:52 +0900

    [ADD] 1238 파티 정답

--- src/baekJoon/advanced/Party.java
+++ src/baekJoon/advanced/Party.java
@@ -12,6 +12,7 @@ import java.util.StringTokenizer;
 public class Party {
     static int N, M, X;
     static List<Node>[] list;
+    static List<Node>[] reverseList;
     static int[] go_dist;
     static int[] back_dist;
     static boolean[] visited;
@@ -31,6 +32,7 @@ public class Party {
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken()) - 1;
         list = new ArrayList[N];
+        reverseList = new ArrayList[N];
         go_dist = new int[N];
         back_dist = new int[N];
         visited = new boolean[N];
@@ -38,6 +40,7 @@ public class Party {
         // 2개를 더한 값 중 최댓값이 정답이 된다.
         for(int i=0; i<N; i++) {
             list[i] = new ArrayList<>();
+            reverseList[i] = new ArrayList<>();
             go_dist[i] = back_dist[i] = Integer.MAX_VALUE;
         }
         go_dist[X] = back_dist[X] = 0;
@@ -49,6 +52,7 @@ public class Party {
             // 기존의 다익스트라였으면 list[destination]... 으로 양방향으로 만들어줘야 하지만,
             // 해당 문제는 양방향이 아니라 각각 단방향마다 다른 가중치를 가지므로 한번만 추가해준다.
             list[start].add(new Node(destination, value));
+            reverseList[destination].add(new Node(start, value));
         }
 
         go_dijkstra();
@@ -61,7 +65,6 @@ public class Party {
     static void go_dijkstra() {
         PriorityQueue<Node> q = new PriorityQueue<>(((o1, o2) -> o1.value - o2.value));
         q.add(new Node(X, 0));
-        go_dist[X] = 0;
         while(!q.isEmpty()) {
             Node now = q.poll();
             if(!visited[now.dest]) {
@@ -82,28 +85,45 @@ public class Party {
         }
     }
 
+//    static void back_dijkstra() {
+//         PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
+//         q.add(new Node(X, 0));
+//
+//         while(!q.isEmpty()) {
+//             Node now = q.poll();
+//
+//             if(!visited[now.dest]) {
+//                 visited[now.dest] = true;
+//             }
+//
+//             for(int i=0; i<N; i++) {
+//                 for(int k=0; k<list[i].size(); k++) {
+//                     if(list[i].get(k).dest == now.dest) {
+//                         if(!visited[i] && back_dist[i] > now.value + list[i].get(k).value) {
+//                             back_dist[i] = now.value + list[i].get(k).value;
+//                             q.add(new Node(list[i].get(k).dest, back_dist[i]));
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//    }
+
     static void back_dijkstra() {
-         PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.value - o2.value);
-         q.add(new Node(X, 0));
-         
-         while(!q.isEmpty()) {
-             Node now = q.poll();
-
-             if(!visited[now.dest]) {
-                 visited[now.dest] = true;
-             }
-
-             for(int i=0; i<N; i++) {
-                 for(int k=0; k<list[i].size(); k++) {
-                     if(list[i].get(k).dest == now.dest) {
-                         if(!visited[i] && back_dist[i] > now.value + list[i].get(k).value) {
-                             back_dist[i] = now.value + list[i].get(k).value;
-                             q.add(new Node(list[i].get(k).dest, back_dist[i]));
-                         }
-                     }
-                 }
-             }
-         }
+        PriorityQueue<Node> q = new PriorityQueue<>(((o1, o2) -> o1.value - o2.value));
+        q.add(new Node(X, 0));
+        while(!q.isEmpty()) {
+            Node now = q.poll();
+            if(!visited[now.dest]) {
+                visited[now.dest] = true;
+            }
+            for (Node next : reverseList[now.dest]) {
+                if(!visited[next.dest] && back_dist[next.dest] > now.value + next.value) {
+                    back_dist[next.dest] = now.value + next.value;
+                    q.add(new Node(next.dest, back_dist[next.dest]));
+                }
+            }
+        }
     }
 
 
