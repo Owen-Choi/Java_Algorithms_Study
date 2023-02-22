fb320068f86ec168b4a088d39023089cd6aa0c23
Owen-Choi <oldstyle482@gmail.com>
2023-02-22 23:00:37 +0900

    [ADD] 귤 고르기

--- src/programmers/level2/Tangerine_Selecting.java
+++ src/programmers/level2/Tangerine_Selecting.java
@@ -1,25 +1,32 @@
 package programmers.level2;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Tangerine_Selecting {
 
     public static void main(String[] args) {
-        int[] tangerine = {1, 1, 1, 1, 1};
+        int[] tangerine = {1,2,3,4,5,6,7};
         int k = 1;
         System.out.println(solution(k, tangerine));
     }
 
     public static int solution(int k, int[] tangerine) {
-        int[] indexes = new int[tangerine.length];
+//        int[] indexes = new int[tangerine.length];
+//        // 오답 원인 : 크기로 주어진 정수가 배열의 길이보다 작다는 보장이 없음.
+//        for (int i : tangerine) {
+//            indexes[i - 1]++;
+//        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
         for (int i : tangerine) {
-            indexes[i - 1]++;
+            map.put(i - 1, map.getOrDefault(i - 1, 0) + 1);
         }
+
+        ArrayList<Integer> list = new ArrayList<>(map.values());
+        Collections.sort(list, Collections.reverseOrder());
         int count = 0;
-        Arrays.sort(indexes);
         while(k > 0) {
-            k -= indexes[tangerine.length - count - 1];
+            k -= list.get(count);
             count++;
         }
         return count;
