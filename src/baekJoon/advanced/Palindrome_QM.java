71a48aa3129744421199ed95f69eb44f7d5b230a
Owen-Choi <oldstyle482@gmail.com>
2023-02-24 17:10:16 +0900

    [ADD] 10942 펠린드롬? dp 풀이 참고

--- src/baekJoon/advanced/Palindrome_QM.java
+++ src/baekJoon/advanced/Palindrome_QM.java
@@ -11,46 +11,57 @@ public class Palindrome_QM {
     static int N;
     static int[] num;
     static int M;
+    static boolean [][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         N = Integer.parseInt(br.readLine());
         num = new int[N];
+        dp = new boolean[N][N];
         st = new StringTokenizer(br.readLine(), " ");
         for(int i=0; i<N; i++) {
             num[i] = Integer.parseInt(st.nextToken());
         }
         M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        solve();
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            sb.append(check(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1)).append('\n');
+            boolean flag = dp[Integer.parseInt(st.nextToken()) - 1][Integer.parseInt(st.nextToken()) - 1];
+            sb.append(flag ? "1" : "0").append('\n');
         }
         System.out.println(sb.toString());
 
     }
 
-    public static int check(int S, int E) {
-        if(S == E) {
-            return 1;
+    public static void solve() {
+        // 길이가 1일때, 항상 펠린드롬이다.
+        for(int i=0; i<N; i++) {
+            dp[i][i] = true;
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i=S; i<=E; i++) {
-            sb.append(num[i]);
+
+        // 길이가 2일때, 11, 22 등과 같이 서로 같아야 펠린드롬이다.
+        for(int i=0; i<N - 1; i++) {
+            if(num[i] == num[i + 1]) {
+                dp[i][i + 1] = true;
+            }
         }
-        String tmp = sb.toString();
-        String firstHalf = tmp.substring(0, tmp.length() / 2);
-        String lastHalf;
-        if(tmp.length() % 2 != 0) {
-            lastHalf = tmp.substring(tmp.length() / 2 + 1, tmp.length());
-        } else {
-            lastHalf = tmp.substring(tmp.length() / 2, tmp.length());
+
+//        for(int i=0; i<N; i++) {
+//            for(int k=1; k<N-i; k++) {
+//                if(num[i] == num[i + k] && dp[i + 1][i + k - 1]) {
+//                    dp[i][i + k] = true;
+//                }
+//            }
+//        }
+
+        for(int i=1; i<N; i++) {
+            for(int k=0; k<N - i; k++) {
+                if(num[k] == num[k + i] && dp[k + 1][k + i - 1]) {
+                    dp[k][k + i] = true;
+                }
+            }
         }
-        StringBuffer stringBuffer = new StringBuffer(lastHalf);
-        String reverse = stringBuffer.reverse().toString();
-        if(reverse.equals(firstHalf)) {
-            return 1;
-        }
-        return 0;
     }
+
 }
