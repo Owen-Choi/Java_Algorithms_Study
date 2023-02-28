6599b1bb5495ad2f0444ccb8e46f5eaac27a9dd2
Owen-Choi <oldstyle482@gmail.com>
2023-03-01 00:52:09 +0900

    [ADD] 줄세우기

--- src/baekJoon/advanced/Making_Line.java
+++ src/baekJoon/advanced/Making_Line.java
@@ -3,9 +3,7 @@ package baekJoon.advanced;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 // 2252번, 줄 세우기, 골드3, Topological Sort
 public class Making_Line {
@@ -24,13 +22,43 @@ public class Making_Line {
             list[i] = new ArrayList<>();
         }
 
+        // 위상정렬
         for(int i=0; i<M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             int firstMan = Integer.parseInt(st.nextToken()) - 1;
             int secondMan = Integer.parseInt(st.nextToken()) - 1;
             list[firstMan].add(secondMan);
+            indegree[secondMan]++;
         }
 
- 
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=0; i< indegree.length; i++) {
+            if(indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        // 위상정렬 로직 시작
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+
+            for (Integer integer : list[node]) {
+                indegree[integer]--;
+
+                if(indegree[integer] == 0) {
+                    queue.add(integer);
+                }
+            }
+        }
+
+        // 결과 출력
+        StringBuilder sb = new StringBuilder();
+        for (Integer integer : result) {
+            sb.append(integer + 1).append(" ");
+        }
+        System.out.println(sb.toString());
     }
 }
