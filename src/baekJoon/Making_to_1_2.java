078bc66d66ee9dcf8df5c419efc3b865aec7d903
Owen-Choi <oldstyle482@gmail.com>
2023-03-05 19:37:55 +0900

    [ADD] 1로 만들기2 오답

--- src/baekJoon/Making_to_1_2.java
+++ src/baekJoon/Making_to_1_2.java
@@ -18,15 +18,25 @@ public class Making_to_1_2 {
 
         for(int i=1; i<N; i++) {
             dp[i] = dp[i - 1] + 1;
+            routeDp[i] = i - 1;
             if(i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+                routeDp[i] = i / 3;
             }
             if(i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+                routeDp[i] = i / 2;
             }
         }
 
         System.out.println(dp[N - 1]);
-        System.out.println(routeDp[N - 1]);
+        int route = N - 1;
+        StringBuilder sb = new StringBuilder();
+        sb.append(N).append(" ");
+        while(route > 0) {
+            sb.append(route).append(" ");
+            route = routeDp[route];
+        }
+        System.out.println(sb.toString());
     }
 }
