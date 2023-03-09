351780b6f0d9786014acbe0e74b301e1c716965e
Owen-Choi <oldstyle482@gmail.com>
2023-03-09 14:01:32 +0900

    [ADD] 조이스틱 풀이 - 로직 변경

--- src/programmers/level2/JoyStick.java
+++ src/programmers/level2/JoyStick.java
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 
 public class JoyStick {
 
-    static int[] alphabets;
     static char[] values;
     static int count = 0;
+    static boolean flag = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String name = br.readLine();
@@ -16,33 +16,48 @@ public class JoyStick {
     }
 
     public static int solution(String name) {
-        alphabets = new int[26];
-        for(int i=0; i<name.length(); i++) {
-            alphabets[i] = i;
-        }
+        int target = 0;
         values = name.toCharArray();
-        for(int i=0; i<name.length() - 1; i++) {
+        for(int i=0; i<name.length(); i = target) {
+            alphabetMove((int)values[i] - 65);
+            if(flag) break;
             // 다음 행선지를 찾고
-            int target = findTarget(i);
+            if(i == name.length() - 1)
+                break;
+            target = findTarget(i);
             // 해당 행선지까지 더 작은 조작으로 이동하고
             cursorMove(i, target);
-            // 도착한 행선지에서 더 작은 조작으로 알파벳 변경
-            alphabetMove((int)values[target] - 65);
-
-            if(target == values.length - 1)
-                break;
         }
+
+//        int target = 0;
+//        int idx = 0;
+//        while(true) {
+//            alphabetMove((int)values[idx] - 65);
+//            if(target == values.length - 1)
+//                break;
+//            target = findTarget(idx);
+//            cursorMove(idx, target);
+//            idx = target;
+//        }
+
         return count;
     }
 
     public static int findTarget(int idx) {
-        int i = idx;
-        while(i < values.length && alphabets[i] == 0) {
+        int i;
+        if(idx == values.length - 1) {
+            // 인덱스 에러 조심
+            i = idx;
+        } else {
+            i = idx + 1;
+        }
+        int moveCount = 1;
+        if(values[i] == 'A') {
+            moveCount++;
             i++;
         }
-        if(i >= values.length)
-            return i - 1;
-        return i;
+
+        return idx + moveCount;
     }
 
     public static void cursorMove(int idx, int target) {
@@ -51,14 +66,13 @@ public class JoyStick {
         // 정방향 크기 계산은 목표지점 - 현재 위치
         // 둘 중에 더 크기가 작은 것을 count에 더해준다.
         count += Math.min(values.length - target + idx, target - idx);
-        System.out.println("move" + " " + Math.min(values.length - target + idx, target - idx));
-
+        if(target == values.length - 1)
+            flag = true;
     }
 
     public static void alphabetMove(int target) {
         // count가 더 작은 쪽으로 더한다.
-        count += Math.min(26 - target + 1, target);
-        System.out.println("target" + " " + Math.min(26 - target + 1, target));
+        count += Math.min(26 - target, target);
     }
 
 
