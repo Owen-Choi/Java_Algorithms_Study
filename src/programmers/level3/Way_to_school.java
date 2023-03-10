83808e873ae1129daab5574e57a137729b582ba7
Owen-Choi <oldstyle482@gmail.com>
2023-03-10 16:42:37 +0900

    [ADD] 등굣길 오답

--- src/programmers/level3/Way_to_school.java
+++ src/programmers/level3/Way_to_school.java
@@ -4,62 +4,66 @@ import java.util.Arrays;
 
 public class Way_to_school {
 
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) {
-        int[][] puddles = {{2,2}};
-        solution(4, 3, puddles);
+        int[][] puddles = {{3,2}, {2,4}};
+//        int[][] puddles = new int[0][0];
+//        System.out.println(solution(4, 3, puddles));
+        System.out.println(solution(100, 100, puddles));
     }
 
     public static int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
         int[][] dp = new int[n][m];
+        int div = 1000000007;
+        for(int i=0; i<n; i++) {
+            Arrays.fill(dp[i], 1);
+        }
 
         for(int i=0; i<n; i++) {
             for(int k=0; k<m; k++) {
-                if(!check(i, k, puddles)) {
+                int flag = check(i, k, puddles);
+                if(flag == 0) {
                     // 이 블럭에 들어온 코드는 주변에 물 웅덩이가 없는 것이다.
                     if(i == 0 && k == 0)
                         continue;
                     if (i == 0) {
                         // 위쪽에서 접근하지 못하는 경우를 체크
                         // 왼쪽에서 들어오는 경우만 더해줌
-                        dp[i][k] = dp[i][k - 1] + 1;
+                        dp[i][k] = dp[i][k - 1];
                     } else if(k == 0) {
                         // 왼쪽에서 접근하지 못하는 경우를 체크
                         // 위쪽에서 들어오는 경우만 더해줌
-                        dp[i][k] = dp[i-1][k] + 1;
+                        dp[i][k] = dp[i-1][k];
                     } else {
-                        dp[i][k] = (dp[i-1][k] + 1) + (dp[i][k - 1] + 1);
+                        dp[i][k] = (dp[i-1][k]) + (dp[i][k - 1]);
                     }
                 } else {
                     // 주변에 물 웅덩이가 있는 경우
-
+                    if(flag == 1) {
+                        // 웅덩이가 위에 있는 경우
+                        dp[i][k] = dp[i][k - 1];
+                    } else if(flag == 2) {
+                        // 웅덩이가 옆에 있는 경우
+                        dp[i][k] = dp[i-1][k];
+                    }
                 }
             }
         }
-        return answer;
+
+        return dp[n - 1][m - 1] % div;
     }
 
-    // 생각해보니 위와 왼쪽만 체크하면 된다.
-//    public static boolean check(int x, int y, int[][] puddles) {
-//        for(int i=0; i<4; i++) {
-//            int nx = x + dx[i];
-//            int ny = y + dy[i];
-//            for(int k=0; k<puddles.length; k++) {
-//                if(nx == puddles[k][0] && ny == puddles[k][1]) {
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//    }
-    public static boolean check(int x, int y, int[][] puddles) {
+    public static int check(int x, int y, int[][] puddles) {
             for(int k=0; k<puddles.length; k++) {
-                if(((x-1) == puddles[k][0] && (y) == puddles[k][1]) || (x == puddles[k][0] && (y - 1) == puddles[k][1])) {
-                    return true;
+                if(((x-1) == puddles[k][0] - 1 && (y) == puddles[k][1] - 1) || (x == puddles[k][0] - 1 && (y - 1) == puddles[k][1] - 1)) {
+                    if((x-1) == puddles[k][0] - 1 && (y) == puddles[k][1] - 1) {
+                        // 웅덩이가 위에 있는 경우 1 반환
+                        return 1;
+                    } else {
+                        // 웅덩이가 옆에 있는 경우 2 반환
+                        return 2;
+                    }
                 }
             }
-        return false;
+        return 0;
     }
 }
