d8e4039079e1c43b278b86495b2a863715c8109c
Owen-Choi <oldstyle482@gmail.com>
2023-03-20 18:21:29 +0900

    [ADD] 카드 섞기 테스트 실패

---
+++ src/baekJoon/advanced/Mixing_Card.java
@@ -0,0 +1,73 @@
+package baekJoon.advanced;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Mixing_Card {
+    static int N;
+    static int[] S, P, value, previous;
+    // 처음 위치로 돌아왔을 경우 -1 출력
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        S = new int[N];
+        P = new int[N];
+        value = new int[N];
+        previous = new int[N];
+        StringTokenizer st;
+
+            st = new StringTokenizer(br.readLine());
+            for(int i=0; i<N; i++) {
+                S[i] = Integer.parseInt(st.nextToken());
+                value[i] = i % 3;
+            }
+            st = new StringTokenizer(br.readLine());
+            for(int v=0; v<N; v++) {
+                P[v] = Integer.parseInt(st.nextToken());
+            }
+
+        int result = 0;
+        while(true) {
+            check(result);
+            mix();
+            result++;
+        }
+
+    }
+
+    static void mix() {
+        Arrays.fill(previous, 0);
+        for(int i=0; i<N; i++) {
+            previous[S[i]] = value[i];
+        }
+        for(int i=0; i<N; i++) {
+            value[i] = previous[i];
+        }
+    }
+
+    static void check(int count) {
+        boolean flag = false;
+        for(int i=0; i<N; i++) {
+            if(value[i] != i % 3) {
+                flag = true;
+                break;
+            }
+        }
+
+        if(!flag && count != 0) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+
+        for(int i=0; i<N; i++) {
+            if(P[i] != value[i]) {
+                return;
+            }
+        }
+        System.out.println(count);
+        System.exit(0);
+    }
+}
