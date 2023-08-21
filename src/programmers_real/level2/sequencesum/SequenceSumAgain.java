6fef3eb07bf7ca1530aa78fd62c3fadd972ad2b0
Owen-Choi <oldstyle482@gmail.com>
2023-08-21 23:37:31 +0900

    [ADD] 연속된 부분 수열의 합 again

--- src/programmers_real/level2/sequencesum/SequenceSumAgain.java
+++ src/programmers_real/level2/sequencesum/SequenceSumAgain.java
@@ -1,25 +1,56 @@
 package programmers_real.level2.sequencesum;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class SequenceSumAgain {
 
     public static void main(String[] args) {
         int[] sequence = {1,1,1,2,3,4,5};
         int sum = 5;
 
-        int[] solution = new Solution_SequenceSum().solution(sequence, sum);
+        int[] solution = new SequenceSumAgainSolution().solution(sequence, sum);
         for (int i : solution) {
             System.out.println(i);
         }
-    }
     }
 }
 
 class SequenceSumAgainSolution {
     public int[] solution(int[] s, int k) {
-        int left = 0, right = 1, value = 0;
-        while(left <= right) {
-
+        int left = 0, right = 0, value = s[0];
+        List<Node> list = new ArrayList<>();
+        while(true) {
+            if(value == k) {
+                list.add(new Node(left, right));
+            }
+            if(left == right && right == s.length)
+                break;
+            if(value <= k && right < s.length) {
+                right++;
+                if(right < s.length)
+                    value += s[right];
+            } else {
+                if(left < s.length) {
+                    value -= s[left++];
+                }
+            }
         }
+        list.sort(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                int length1 = o1.end - o1.start;
+                int length2 = o2.end - o2.start;
+                if(length1 == length2) {
+                    return o1.start - o2.start;
+                }
+                return length1 - length2;
+            }
+        });
+
+        int[] answer = {list.get(0).start, list.get(0).end};
+        return answer;
     }
 
     class Node {
