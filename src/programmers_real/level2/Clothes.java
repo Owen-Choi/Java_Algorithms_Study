f99b4b2bdf4d83eac407ed640b81423794200ac9
Owen-Choi <oldstyle482@gmail.com>
2023-08-29 02:52:44 +0900

    [ADD] 의상 (타인 풀이 참고)

--- src/programmers_real/level2/Clothes.java
+++ src/programmers_real/level2/Clothes.java
@@ -8,14 +8,14 @@ import java.util.Map;
 public class Clothes {
     public static void main(String[] args) {
         String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        new ClothesSolution().solution(clothes);
+        System.out.println(new ClothesSolution().solution(clothes));
     }
 }
 
 class ClothesSolution {
     Map<String, List<String>> map = new HashMap<>();
     public int solution(String[][] clothes) {
-        int answer = 0;
+        int answer = 1;
         for(int i=0; i<clothes.length; i++) {
             if(!map.containsKey(clothes[i][1]))
                 map.computeIfAbsent(clothes[i][1], v -> new ArrayList<>()).add(clothes[i][0]);
@@ -23,7 +23,9 @@ class ClothesSolution {
                 map.get(clothes[i][1]).add(clothes[i][0]);
         }
 
-
-        return answer;
+        for (List<String> value : map.values()) {
+            answer *= (value.size() + 1);
+        }
+        return answer - 1;
     }
 }
