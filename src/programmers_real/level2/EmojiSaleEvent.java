de39e2f30871a458583211b97b02b90fcebd3eac
Owen-Choi <oldstyle482@gmail.com>
2023-08-30 15:34:20 +0900

    [ADD] 이모티콘 할인 행사

--- src/programmers_real/level2/EmojiSaleEvent.java
+++ src/programmers_real/level2/EmojiSaleEvent.java
@@ -1,5 +1,9 @@
 package programmers_real.level2;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 // https://school.programmers.co.kr/learn/courses/30/lessons/150368
 public class EmojiSaleEvent {
     public static void main(String[] args) {
@@ -15,21 +19,79 @@ public class EmojiSaleEvent {
 class EmojiSaleEventSolution {
     int plusMax = 0;
     int priceMax = 0;
+
+    List<int[]> result = new ArrayList<>();
     int[][] u;
     int[] e;
     public int[] solution(int[][] users, int[] emoticons) {
-        int[] answer = {};
         // 재귀를 통해서 모든 이모티콘에 대한 모든 할인율을 조사해보자.
         e = emoticons;
         u = users;
-        return answer;
+
+        for(int i=10; i<=40; i+=10) {
+            recur(new int[users.length], i, 0);
+        }
+
+        // 재귀를 돌면서 업데이트된 result를 통해서 정답을 도출
+        List<Node> list = new ArrayList<>();
+        for (int[] ints : result) {
+            int member, price;
+            member = price = 0;
+            for(int i=0; i<ints.length; i++) {
+                if(ints[i] >= users[i][1]) {
+                    member++;
+                } else {
+                    price += ints[i];
+                }
+            }
+            Node node = new Node(member, price);
+            list.add(node);
+        }
+
+        list.sort(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                if(o1.plusMember == o2.plusMember)
+                    return o2.totalPrice - o1.totalPrice;
+                return o2.plusMember - o1.plusMember;
+            }
+        });
+
+        return new int[] {list.get(0).plusMember, list.get(0).totalPrice};
     }
 
-    public void recur() {
-        // 종료 컨디션
+    void recur(int[] price, int discount, int depth) {
 
-        for(int i=0; i<e.length; i++) {
-            
+        if(depth == e.length) {
+            result.add(price);
+            return;
+        }
+
+        int[] newPrice = new int[price.length];
+        for(int i=0; i<newPrice.length; i++) {
+            newPrice[i] = price[i];
+        }
+
+        for(int i=0; i<u.length; i++) {
+            if(u[i][0] <= discount) {
+                // 사용자가 원하는 할인율을 넘었을 경우 구매
+                newPrice[i] += e[depth] * (100 - discount) / 100;
+            }
+        }
+
+        for(int i=10; i<=40; i+= 10) {
+            recur(newPrice, i, depth + 1);
+        }
+
+    }
+
+    class Node{
+        int plusMember;
+        int totalPrice;
+
+        public Node(int plusMember, int totalPrice) {
+            this.plusMember = plusMember;
+            this.totalPrice = totalPrice;
         }
     }
 }
