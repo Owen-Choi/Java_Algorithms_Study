49f856050355c791ec86323ed0524b921d3a52b7
Owen-Choi <oldstyle482@gmail.com>
2024-01-03 23:18:34 +0900

    [ADD] 수강신청

---
+++ src/baekJoon/normal/출석체크.java
@@ -0,0 +1,12 @@
+package baekJoon.normal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 출석체크 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
